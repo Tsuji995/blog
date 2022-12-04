990ae8c1e724b1d1f381a4d5c083dc9248bf1c33
tsuji <shizhicheng651@gmail.com>
2022-12-04 16:44:05 +0900

    default export

--- components/two-column.js
+++ components/two-column.js
@@ -1,6 +1,6 @@
 import styles from 'styles/two-column.module.css'
 
-export function TwoColumn({ children }) {
+export default function TwoColumn({ children }) {
     return (
         <div className={styles.flexContainer}>
             {children}
@@ -8,7 +8,7 @@ export function TwoColumn({ children }) {
     )
 }
 
-export function TwoColumnMain({ children }) {
+export default function TwoColumnMain({ children }) {
     return (
         <div className={styles.main}>
             {children}
@@ -16,7 +16,7 @@ export function TwoColumnMain({ children }) {
     )
 }
 
-export function TwoColumnSidebar({ children }) {
+export default function TwoColumnSidebar({ children }) {
     return (
         <div className={styles.sidebar}>
             {children}
